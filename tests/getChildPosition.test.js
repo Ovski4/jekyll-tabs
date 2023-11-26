e7e6e2b760f15df592233b182091c0d22857ea1a
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2023-11-25 20:56:31 -0800

    Update test

--- tests/getChildPosition.test.js
+++ tests/getChildPosition.test.js
@@ -1,21 +1,19 @@
-const { getChildPosition, createElementFromHtml } = require('../js/domHelpers');
+const { getChildPosition } = require('../js/domHelpers');
 
-const listMarkup = `
+document.body.innerHTML = `
     <ul>
-        <li id="alpha"></li>
-        <li id="omega"></li>
-        <li id="lambda"></li>
-        <li id="beta"></li>
+        <li class="alpha"></li>
+        <li class="omega"></li>
+        <li class="lambda"></li>
+        <li class="beta"></li>
     </ul>`
 ;
 
-const listElement = createElementFromHtml(listMarkup);
-
 describe('Get the child position from the point of view of its parent element', () => {
     it('Should return the right li positions in a ul element', () => {
-        expect(getChildPosition(listElement.querySelector('[id="alpha"]'))).toBe(0);
-        expect(getChildPosition(listElement.querySelector('[id="omega"]'))).toBe(1);
-        expect(getChildPosition(listElement.querySelector('[id="lambda"]'))).toBe(2);
-        expect(getChildPosition(listElement.querySelector('[id="beta"]'))).toBe(3);
+        expect(getChildPosition(document.querySelector('.alpha'))).toBe(0);
+        expect(getChildPosition(document.querySelector('.omega'))).toBe(1);
+        expect(getChildPosition(document.querySelector('.lambda'))).toBe(2);
+        expect(getChildPosition(document.querySelector('.beta'))).toBe(3);
     });
 });
