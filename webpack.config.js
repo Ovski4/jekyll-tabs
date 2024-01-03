9065c19b2e67308853609758fae123acca190a0f
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2024-01-03 12:52:45 -0800

    Add typescript support

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
 const path = require('path');
 
 module.exports = {
-    entry: './js/jekyllTabs.js',
+    entry: './js/jekyllTabs.ts',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
     output: {
         filename: 'tabs.js',
         path: path.resolve(__dirname, 'docs'),
