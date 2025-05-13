7a1d3940dc76a7c0b382f4306b1c142ea8610fd1
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2025-05-12 21:04:37 -0700

    Simplify helper logic

--- tests/testHelper.js
+++ tests/testHelper.js
@@ -1,25 +1,23 @@
 const mockWindowLocationProperties = (value, replaceStateMock = jest.fn()) => {
-    // eslint-disable-next-line no-global-assign
-    window = Object.create(window);
-
-    Object.defineProperty(window, 'location', {
-        value,
-        writable: true,
-    });
-
-    Object.defineProperty(window, 'history', {
-        value: {
-            replaceState: replaceStateMock,
+    Object.defineProperties(window, {
+        location: {
+            value,
+            writable: true,
         },
-        writable: false,
+        history: {
+            value: {
+                replaceState: replaceStateMock,
+            },
+            writable: false,
+        },
     });
-}
+};
 
 const removeAllWhitespaces = (string) => {
     return string.replace(/\s/g, '');
-}
+};
 
 module.exports = {
     mockWindowLocationProperties,
-    removeAllWhitespaces
-}
+    removeAllWhitespaces,
+};
