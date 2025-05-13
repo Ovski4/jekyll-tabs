4339c0825db79bc8f8fb7e558726959a1ca000ac
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2025-05-12 20:56:12 -0700

    Fix lints and tweak eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,41 @@
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  tseslint.configs.recommended,
+    globalIgnores([
+        'docs/',
+    ]),
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            sourceType: "commonjs",
+            globals: {
+                ...globals.node,
+                ...globals.jest
+            }
+        }
+    },
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        languageOptions: {
+            globals: globals.browser
+        }
+    },
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        plugins: { js },
+        extends: ["js/recommended"]
+    },
+    {
+        files: ["**/*.test.js"],
+        languageOptions: { globals: globals.jest }
+    },
+    tseslint.configs.recommended,
+    {
+        rules: {
+            '@typescript-eslint/no-require-imports': 'off',
+        },
+    },
 ]);
