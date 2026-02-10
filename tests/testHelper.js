073abe8017206f397645374e3322690e204d42f9
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2026-02-09 20:14:38 -0800

    Add a note about jsdom causing difficulties to update

--- tests/testHelper.js
+++ tests/testHelper.js
@@ -1,3 +1,8 @@
+/**
+ * Note that upgrading to jsdom >= 30 causes the following error: "TypeError: Cannot redefine property: location"
+ * Here are some nice workarounds suggested in the related issue: https://github.com/jsdom/jsdom/issues/3492
+ * Decided to not upgrade jsdom for now since it's only impacting the tests.
+ */
 const mockWindowLocationProperties = (value, replaceStateMock = jest.fn()) => {
     Object.defineProperties(window, {
         location: {
